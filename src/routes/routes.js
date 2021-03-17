3b2f77489e79c77b62e0ff4c17b4b42c68807c02
AngelVillaDev <villalopezluis99@gmail.com>
2021-03-17 14:36:09 -0700

    change data

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,10 +5,17 @@ const router = Router();
 
 const express = require(express);
 const router = express.router();
-
-**/
 router.get('/', (req, res) => {
     res.json({"name":"Jhon Doe 2"});
 });
+
+**/
+router.get('/', (req,res) => {
+    const data = {
+        "name":"Jhon Doe",
+        "id":"1234"
+    }
+    res.json(data);
+})
 
 module.exports = router;
