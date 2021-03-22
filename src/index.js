622070be3d99954416cdc83e178932887f5422a2
AngelVillaDev <villalopezluis99@gmail.com>
2021-03-22 03:02:22 -0700

    Dta and routes changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 
 
+const { json } = require('express');
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
@@ -7,13 +8,14 @@ const morgan = require('morgan');
 //Configurations
 
 app.set('port',3000);
+app.set('json spaces',2)
 
 //Middlewares
 app.use(morgan('dev'));
 app.use(express.json());
 
 //paths
-app.use(require('./routes/routes'));
+app.use('/api',require('./routes/routes'));
 
 //Server Initializer 
 
